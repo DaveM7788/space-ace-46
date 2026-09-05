bfbd2d24b203b0cd516d83e27320e06a7d5a6e00
DaveM7788 <contact@fittestfire.com>
2026-09-04 20:00:08 -0500

    add player health boosts
    also enemies can spawn missles randomly upon destroy

--- Globals/SignalHub.gd
+++ Globals/SignalHub.gd
@@ -3,6 +3,7 @@ extends Node
 
 signal spawn_pool_object(position: Vector2, scene: PackedScene)
 signal player_take_damage(damage: int)
+signal player_health_boost(boost: int)
 
 func emit_spawn_pool_object(position: Vector2, scene: PackedScene) -> void:
 	spawn_pool_object.emit(position, scene)
@@ -10,3 +11,7 @@ func emit_spawn_pool_object(position: Vector2, scene: PackedScene) -> void:
 
 func emit_player_take_damage(damage: int) -> void:
 	player_take_damage.emit(damage)
+
+
+func emit_player_health_boost(boost: int) -> void:
+	player_health_boost.emit(boost)
--- Scenes/PowerUp/power_up.gd
+++ Scenes/PowerUp/power_up.gd
@@ -1,5 +1,18 @@
 class_name PowerUp extends Poolable
 
+@export var boost := 25
+@onready var life_timer: Timer = $LifeTimer
+
+
+func activate() -> void:
+	super()
+	life_timer.start()
+
 
 func _on_life_timer_timeout() -> void:
+	deactivate()
+
+
+func _on_detection_area_entered(area: Area2D) -> void:
+	SignalHub.emit_player_health_boost(boost)
 	deactivate()
