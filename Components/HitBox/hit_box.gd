8fd3f9e7481efca82d9bd248dc911904cca24d44
DaveM7788 <contact@fittestfire.com>
2026-09-04 20:14:51 -0500

    invince homing missle
    if enemy destruction results in missle. give missle some iframes

--- Components/HitBox/hit_box.gd
+++ Components/HitBox/hit_box.gd
@@ -14,6 +14,9 @@ var _collisions_left := 1
 		if is_node_ready():
 			apply_shape()
 
+@onready var invincibility_timer: Timer = $InvincibilityTimer
+@export var invincible_time := 0.1
+var _invincible := false
 
 func _ready() -> void:
 	apply_shape()
@@ -26,9 +29,13 @@ func apply_shape() -> void:
 
 func reset() -> void:
 	_collisions_left = max_collisions
+	invincibility_timer.start(invincible_time)
+	_invincible = true
 
 
 func _on_area_entered(area: Area2D) -> void:
+	if _invincible: return
+	
 	if area is HitBox:
 		hit.emit(area.damage_dealt)
 
@@ -36,3 +43,7 @@ func _on_area_entered(area: Area2D) -> void:
 	if _collisions_left <= 0:
 		died.emit(area)
 		print(get_parent().name + " 0 collisions left hitbox")
+
+
+func _on_invincibility_timer_timeout() -> void:
+	_invincible = false
